e595af1e6b9d88d131c87a8228ed6da3e808322d
botfiletolink <129716967+botfiletolink@users.noreply.github.com>
2024-12-16 05:54:00 +0100

    Update confi.py

--- confi.py
+++ confi.py
@@ -1,3 +1,3 @@
-Bot_token = "6607617536:AAHmlFhwr6ayjGviYGg6aeAc7Kf6b_Frxvg"
-chanel_id = -1001847478446
+Bot_token = "7877325625:AAGFeY-5YOLNSBuH9NqFy5znkl-JxqhOPCw"
+chanel_id = -1002305957185
 db_access = 55555555555 #value int de la db, desmarcar line 1531 bot.py
